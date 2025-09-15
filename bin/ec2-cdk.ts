ee5d4e7fc8d0fc3d95194f758b8ea9fe2c1cbfee
nikeda <4104cafeaulait@gmail.com>
2025-09-15 16:09:16 +0900

    fix: 認証エラー

--- bin/ec2-cdk.ts
+++ bin/ec2-cdk.ts
@@ -3,24 +3,18 @@ import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { Ec2CdkStack } from '../lib/ec2-cdk-stack';
 import { S3CloudfrontStack } from '../lib/s3-cloudfront-stack';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 const app = new cdk.App();
-
-const account = process.env.AWS_ACCOUNT_ID;
-const region = process.env.AWS_REGION;
 
 new Ec2CdkStack(app, 'Ec2CdkStack', {
   env: {
-    account: account,
-    region: region,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 });
 
 new S3CloudfrontStack(app, 'S3CloudfrontStack', {
   env: {
-    account: account,
-    region: region,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 });
